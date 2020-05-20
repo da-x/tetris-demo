abbb73695b25653b98e8773de94f4169fa0c23da
Dan Aloni <alonid@gmail.com>
2020-05-20 22:47:14 +0300

    Empty window and main loop
    Now we have an empty window, and a main loop that does nothing.

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,10 @@
+use piston_window::{WindowSettings, PistonWindow};
+
 fn main() {
+    let mut window: PistonWindow = WindowSettings::new(
+        "Tetris", [300, 800]).exit_on_esc(true).build().unwrap();
+
+    while let Some(e) = window.next() {
+        println!("{:?}", e);
+    }
 }
